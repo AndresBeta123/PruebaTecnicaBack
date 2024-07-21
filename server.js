cb08d9c18a13e95439d06402bfe2ff6c2a41ed6a
Andres <abetancurthb@unal.edu.co>
2024-07-21 11:14:49 -0500

    hashedPassword

--- server.js
+++ server.js
@@ -1,44 +1,48 @@
-const express = require('express')
-const pool = require('./db')
-const port = 3000
+const express = require('express');
+const bcrypt = require('bcrypt');
+const pool = require('./db');
+const port = 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-//routes
-app.get('/',async (req,res) => {
-    try{
-        const data = await pool.query("SELECT * FROM users")
-        res.status(200).send(data.rows) 
-    }catch(err){
-        console.log(err)
-        res.sendStatus(500)
+// Routes
+app.get('/', async (req, res) => {
+    try {
+        const data = await pool.query("SELECT * FROM users");
+        res.status(200).json(data.rows);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
     }
-})
+});
 
-app.post('/',async (req,res)=>{
-    const {name,userName,password} = req.body
-    try{
-        await pool.query("INSERT INTO users (name,userName,password) VALUES($1,$2,$3)",[name,userName,password])
-        res.status(200).send({
-            message:`YOUR KEYS WERE ${name}-${userName}-${password}`
-        })
-    }catch(err){
-        console.log(err)
-        res.sendStatus(500)
+app.post('/', async (req, res) => {
+    const { name, userName, password } = req.body;
+    try {
+        const salt = await bcrypt.genSalt();  // Corregido de gensalt a genSalt
+        const hashedPassword = await bcrypt.hash(password, salt);
+        console.log(hashedPassword);
+        await pool.query("INSERT INTO users (name, userName, password) VALUES ($1, $2, $3)", [name, userName, hashedPassword]);
+        res.status(200).json({
+            message: `User created with name: ${name}, userName: ${userName}`
+        });
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
     }
-    
-})
+});
 
-app.get('/setup',async(req,res)=>{
-    try{
-        await pool.query('CREATE TABLE users( id SERIAL PRIMARY KEY, name VARCHAR(100), userName Varchar(100),password Varchar(250))')
-        res.status(200).send({
-            message:"se creo la tabla"
-        })
-    }catch(err){
-        console.log(err)
-        res.sendStatus(500)
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), userName VARCHAR(100), password VARCHAR(250) CONSTRAINT uniqueUserName UNIQUE (userName))');
+        res.status(200).json({
+            message: "Table created successfully"
+        });
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
     }
-})
-app.listen(port ,() => console.log(`Server has started on port: ${port}`))
+});
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
