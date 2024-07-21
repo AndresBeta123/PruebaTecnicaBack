8233f983e3d414a927ea38333f9e1a6cb1ed22c9
Andres <abetancurthb@unal.edu.co>
2024-07-21 10:38:38 -0500

    primeraPruebaBaseDeDatosDocker

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const pool = require('./db')
-const port = 1337
-const app = express()
+const port = 3000
 
-app.get('/', (req,res) => {
-    res.sendStatus(200)
+const app = express()
+app.use(express.json())
+
+//routes
+app.get('/',async (req,res) => {
+    try{
+        const data = await pool.query("SELECT * FROM users")
+        res.status(200).send(data.rows) 
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
+app.post('/',async (req,res)=>{
+    const {name,userName,password} = req.body
+    try{
+        await pool.query("INSERT INTO users (name,userName,password) VALUES($1,$2,$3)",[name,userName,password])
+        res.status(200).send({
+            message:`YOUR KEYS WERE ${name}-${userName}-${password}`
+        })
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+    
+})
+
+app.get('/setup',async(req,res)=>{
+    try{
+        await pool.query('CREATE TABLE users( id SERIAL PRIMARY KEY, name VARCHAR(100), userName Varchar(100),password Varchar(250))')
+        res.status(200).send({
+            message:"se creo la tabla"
+        })
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 app.listen(port ,() => console.log(`Server has started on port: ${port}`))
