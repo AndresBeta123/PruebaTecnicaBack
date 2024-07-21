9306bc755008b2cbfed22e666734b22ed21648c9
Andres <abetancurthb@unal.edu.co>
2024-07-21 11:33:11 -0500

    RegistroyLogin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get('/', async (req, res) => {
+app.get('/users', async (req, res) => {
     try {
         const data = await pool.query("SELECT * FROM users");
         res.status(200).json(data.rows);
@@ -17,7 +17,33 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.post('/', async (req, res) => {
+app.post('/login', async (req, res) => {
+    const { userName, password } = req.body;
+
+    try {
+        // Buscar el usuario por nombre de usuario
+        const result = await pool.query('SELECT * FROM users WHERE "username" = $1', [userName]);
+        const user = result.rows[0];
+
+        if (!user) {
+            // Usuario no encontrado
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (await bcrypt.compare(password, user.password)) {
+            // Contraseña correcta
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            // Contraseña incorrecta
+            res.status(401).json({ message: 'Invalid password' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/register', async (req, res) => {
     const { name, userName, password } = req.body;
     try {
         const salt = await bcrypt.genSalt();  // Corregido de gensalt a genSalt
@@ -35,7 +61,7 @@ app.post('/', async (req, res) => {
 
 app.get('/setup', async (req, res) => {
     try {
-        await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), userName VARCHAR(100), password VARCHAR(250) CONSTRAINT uniqueUserName UNIQUE (userName))');
+        await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), userName VARCHAR(100) UNIQUE, password VARCHAR(250))');
         res.status(200).json({
             message: "Table created successfully"
         });
